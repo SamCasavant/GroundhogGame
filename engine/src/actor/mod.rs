4ca01fabe9846cd7d96de1ca026fe6d470af64ed
Samuel Casavant <samuel.j.casavant@gmail.com>
2021-09-30 04:51:51 -0400

    New formatting

--- engine/src/actor/mod.rs
+++ engine/src/actor/mod.rs
@@ -1,109 +1,116 @@
-use crate::world;
 use bevy::prelude::*;
 
+use crate::world;
+
 pub struct Routine {
-    tasks: Option<Vec<ScheduledTask>>,
+   tasks: Option<Vec<ScheduledTask>>,
 }
 
 pub struct ScheduledTask {
-    task: Task,
-    time: world::time::GameTime,
+   task: Task,
+   time: world::time::GameTime,
 }
 
 #[derive(Copy, Clone)]
 pub struct Task {
-    action: Action,
-    parameters: ActionParameters,
-    priority: u32,
+   action:     Action,
+   parameters: ActionParameters,
+   priority:   u32,
 }
 
 #[derive(Copy, Clone)]
 pub enum Action {
-    Wait,
-    Eat,
+   Wait,
+   Eat,
 }
 
 #[derive(Default, Copy, Clone)]
 pub struct ActionParameters {
-    location: Option<world::Position>,
-    target: Option<Entity>,
+   location: Option<world::Position>,
+   target:   Option<Entity>,
 }
 
-pub struct Intelligent; //Intelligent actor component
+pub struct Intelligent; // Intelligent actor component
 
 pub struct Status {
-    //Used for keeping track of actor state, values are primarily used for priority of subsequent action
-    hunger: u32,
-    laziness: u32, //Actor will prefer inaction over actions with lower priority than laziness
+   // Used for keeping track of actor state, values are primarily used for priority of subsequent action
+   hunger:   u32,
+   laziness: u32, /* Actor will prefer inaction over actions with lower priority than laziness */
 }
 
 pub struct ActorPlugin;
 
 impl Plugin for ActorPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.insert_resource(AnimalTimer(world::time::GameTime::from_stamp(
-            world::time::Stamp {
-                day: 0,
-                hour: 6,
-                minute: 0,
-                second: 0,
-            },
-        )))
-        .add_system(animal_processes.system().label("preparation"))
-        .add_system(choose_next_task.system().label("planning"));
-    }
+   fn build(
+      &self,
+      app: &mut AppBuilder,
+   ) {
+      app.insert_resource(AnimalTimer(world::time::GameTime::from_stamp(
+         world::time::Stamp {
+            day:    0,
+            hour:   6,
+            minute: 0,
+            second: 0,
+         },
+      )))
+      .add_system(animal_processes.system().label("preparation"))
+      .add_system(choose_next_task.system().label("planning"));
+   }
 }
 
 fn choose_next_task(
-    mut commands: Commands,
-    mut query: Query<(Entity, &Status, &Routine), (With<Intelligent>, Without<Task>)>,
-    time: Res<world::time::GameTime>,
+   mut commands: Commands,
+   mut query: Query<
+      (Entity, &Status, &Routine),
+      (With<Intelligent>, Without<Task>),
+   >,
+   time: Res<world::time::GameTime>,
 ) {
-    for (entity, status, routine) in query.iter_mut() {
-        let mut curtask = Task {
-            action: Action::Wait,
+   for (entity, status, routine) in query.iter_mut() {
+      let mut curtask = Task {
+         action:     Action::Wait,
+         parameters: ActionParameters {
+            ..Default::default()
+         },
+         priority:   status.laziness,
+      };
+      // TODO: Change priority calculation for scheduled events to incorporate eta
+      match &routine.tasks {
+         Some(tasks) => {
+            let priority = time.how_soon(&tasks[0].time);
+            if priority > curtask.priority {
+               curtask = tasks[0].task.clone();
+            }
+         }
+         None => todo!(),
+      }
+      if status.hunger > curtask.priority {
+         curtask = Task {
+            action:     Action::Eat,
             parameters: ActionParameters {
-                ..Default::default()
+               ..Default::default()
             },
-            priority: status.laziness,
-        };
-        //TODO: Change priority calculation for scheduled events to incorporate eta
-        match &routine.tasks {
-            Some(tasks) => {
-                let priority = time.how_soon(&tasks[0].time);
-                if priority > curtask.priority {
-                    curtask = tasks[0].task.clone();
-                }
-            }
-            None => todo!(),
-        }
-        if status.hunger > curtask.priority {
-            curtask = Task {
-                action: Action::Eat,
-                parameters: ActionParameters {
-                    ..Default::default()
-                },
-                priority: status.hunger,
-            }
-        }
-        commands.entity(entity).insert(curtask);
-    }
+            priority:   status.hunger,
+         }
+      }
+      commands.entity(entity).insert(curtask);
+   }
 }
 
-struct Animal; //Component marker for animals (including humans)
+struct Animal; // Component marker for animals (including humans)
 
 struct AnimalTimer(world::time::GameTime);
 
 fn animal_processes(
-    //Updates animal-inherent statuses; hunger, thirst, etc.
-    mut query: Query<&mut Status, With<Animal>>,
-    game_time: Res<world::time::GameTime>,
-    mut timer: ResMut<AnimalTimer>,
+   // Updates animal-inherent statuses; hunger, thirst, etc.
+   mut query: Query<&mut Status, With<Animal>>,
+   game_time: Res<world::time::GameTime>,
+   mut timer: ResMut<AnimalTimer>,
 ) {
-    if timer.0 <= *game_time {
-        for mut status in query.iter_mut() {
-            status.hunger += 1;
-        }
-        *timer = AnimalTimer(game_time.copy_and_tick(60));
-    }
+   if timer.0 <= *game_time {
+      for mut status in query.iter_mut() {
+         status.hunger += 1;
+      }
+      *timer = AnimalTimer(game_time.copy_and_tick(60));
+   }
 }
--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,7 +1,5 @@
-/*
-The engine is responsible for building and operating the game.
-This file should be an interface between the engine and external world building scripts.
-*/
+// The engine is responsible for building and operating the game.
+// This file should be an interface between the engine and external world building scripts.
 
 use bevy::prelude::*;
 pub mod movement;
@@ -13,46 +11,46 @@ pub use bevy_ecs_tilemap::prelude::*;
 
 #[derive(Bundle)]
 struct ActorComponents {
-    spritesheet: SpriteSheetBundle,
-    position: world::Position,
-    identity: Identity,
-    destination: world::Destination,
+   spritesheet: SpriteSheetBundle,
+   position:    world::Position,
+   identity:    Identity,
+   destination: world::Destination,
 }
 
 pub fn spawn_actor(
-    commands: &mut Commands,
-    identity: Identity,
-    position: world::Position,
-    destination: world::Destination,
-    sprite_sheet: SpriteSheetBundle,
+   commands: &mut Commands,
+   identity: Identity,
+   position: world::Position,
+   destination: world::Destination,
+   sprite_sheet: SpriteSheetBundle,
 ) {
-    commands
-        .spawn()
-        .insert(identity)
-        .insert(position)
-        .insert(movement::movement::Orientation(
-            movement::movement::Direction::Down,
-        ))
-        .insert(destination)
-        .insert_bundle(sprite_sheet)
-        .insert(world::time::GameTime::from_stamp(world::time::Stamp {
-            day: 0,
-            hour: 6,
-            minute: 0,
-            second: 0,
-        }));
+   commands
+      .spawn()
+      .insert(identity)
+      .insert(position)
+      .insert(movement::movement::Orientation(
+         movement::movement::Direction::Down,
+      ))
+      .insert(destination)
+      .insert_bundle(sprite_sheet)
+      .insert(world::time::GameTime::from_stamp(world::time::Stamp {
+         day:    0,
+         hour:   6,
+         minute: 0,
+         second: 0,
+      }));
 }
 
 #[derive(Clone)]
 pub struct Identity {
-    pub specific: bool,
-    pub name: String,
+   pub specific: bool,
+   pub name:     String,
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+   #[test]
+   fn it_works() {
+      assert_eq!(2 + 2, 4);
+   }
 }
