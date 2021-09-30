9a15f21e84f63042d7e36832ff920dafbc0b04d6
Samuel Casavant <samuel.j.casavant@gmail.com>
2021-09-30 06:32:59 -0400

    Various clippy changes

--- engine/src/world/mod.rs
+++ engine/src/world/mod.rs
@@ -5,11 +5,12 @@
 // Note: Support for bevy_ecs_tilemap/tiled_map to be deprecated in future
 //
 // Pathfinding:
-// Entities with a Position and Destinations component, but without a Path component use this module to generate a path.
-// Paths are initialized in full using aStar.
-// Paths are stored in the Path component (a vector of positions) and
-// Ground Types are used to produce tile weights, which hopefully can encourage aStar to prefer sidewalks over roads.
-// Note: This may not be deterministic, and needs to be. Consider invoking bevy stages.
+// Entities with a Position and Destinations component, but without a Path
+// component use this module to generate a path. Paths are initialized in full
+// using aStar. Paths are stored in the Path component (a vector of positions)
+// and Ground Types are used to produce tile weights, which hopefully can
+// encourage aStar to prefer sidewalks over roads. Note: This may not be
+// deterministic, and needs to be. Consider invoking bevy stages.
 //
 
 use std::collections::HashMap;
@@ -26,50 +27,50 @@ pub struct Path(pub Vec<Position>);
 
 #[derive(Debug)]
 pub enum GroundType {
-   ShortGrass,
-   TallGrass,
-   Sidewalk,
-   Path,
-   Street,
-   Crosswalk,
-   Obstacle,
+    ShortGrass,
+    TallGrass,
+    Sidewalk,
+    Path,
+    Street,
+    Crosswalk,
+    Obstacle,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Hash, Eq)]
 pub struct Position {
-   pub x: i64,
-   pub y: i64,
+    pub x: i64,
+    pub y: i64,
 }
 
 pub struct Destination(pub Position);
 
 impl PartialEq<Position> for Destination {
-   fn eq(
-      &self,
-      other: &Position,
-   ) -> bool {
-      self.0.x == other.x && self.0.y == other.y
-   }
+    fn eq(
+        &self,
+        other: &Position,
+    ) -> bool {
+        self.0.x == other.x && self.0.y == other.y
+    }
 }
 
 #[derive(Debug)]
 pub struct Tile {
-   pub occupied:    bool,
-   pub ground_type: GroundType,
+    pub occupied:    bool,
+    pub ground_type: GroundType,
 }
 #[derive(Default)]
 pub struct TileMap {
-   pub map: HashMap<Position, Tile>,
+    pub map: HashMap<Position, Tile>,
 }
 
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
-   fn build(
-      &self,
-      app: &mut AppBuilder,
-   ) {
-      app
+    fn build(
+        &self,
+        app: &mut AppBuilder,
+    ) {
+        app
             //Tilemap
             .insert_resource(TileMap {
                 map: HashMap::<Position, Tile>::new(),
@@ -84,92 +85,95 @@ impl Plugin for WorldPlugin {
             .add_startup_system(init_tilemaps.system())
             .add_system(plan_path.system().label("preparation"))
             .add_plugin(time::TimePlugin);
-   }
+    }
 }
 
 fn init_tilemaps(
-   mut commands: Commands,
-   asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
 ) {
-   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
-   let handle: Handle<TiledMap> = asset_server.load("maps/test.tmx");
+    let handle: Handle<TiledMap> = asset_server.load("maps/test.tmx");
 
-   let map_entity = commands.spawn().id();
+    let map_entity = commands.spawn().id();
 
-   commands.entity(map_entity).insert_bundle(TiledMapBundle {
-      tiled_map: handle,
-      map: Map::new(0u16, map_entity),
-      transform: Transform::from_xyz(0.0, 0.0, 0.0)
-         .mul_transform(Transform::from_scale(Vec3::splat(4.0))),
-      ..Default::default()
-   });
+    commands.entity(map_entity).insert_bundle(TiledMapBundle {
+        tiled_map: handle,
+        map: Map::new(0_u16, map_entity),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0)
+            .mul_transform(Transform::from_scale(Vec3::splat(4.0))),
+        ..Default::default()
+    });
 }
 
 fn plan_path(
-   mut commands: Commands,
-   query: Query<(Entity, &Position, &Destination), Without<Path>>,
-   tilemap: ResMut<TileMap>,
+    mut commands: Commands,
+    query: Query<(Entity, &Position, &Destination), Without<Path>>,
+    tilemap: ResMut<TileMap>,
 ) {
-   for (entity, position, destination) in query.iter() {
-      let plan = astar(
-         position,
-         |p| move_weights(p, &tilemap),
-         |p| {
-            (absdiff(p.x, destination.0.x) + absdiff(p.y, destination.0.y))
-               .try_into()
-               .unwrap()
-         },
-         |p| *p == destination.0,
-      );
-      if let Some(p) = plan {
-         let mut path = p.0;
-         path.remove(0);
-         if path.len() > 0 {
-            commands.entity(entity).insert(Path(path));
-         }
-      }
-   }
+    for (entity, position, destination) in query.iter() {
+        let plan = astar(
+            position,
+            |p| move_weights(p, &tilemap),
+            |p| {
+                (absdiff(p.x, destination.0.x) + absdiff(p.y, destination.0.y))
+                    .try_into()
+                    .expect(
+                        "Two absolute values summed negative, or map is too \
+                         big.",
+                    )
+            },
+            |p| *p == destination.0,
+        );
+        if let Some(p) = plan {
+            let mut path = p.0;
+            path.remove(0);
+            if !path.is_empty() {
+                commands.entity(entity).insert(Path(path));
+            }
+        }
+    }
 }
 
 pub fn move_weights(
-   position: &Position,
-   tilemap: &ResMut<TileMap>,
+    position: &Position,
+    tilemap: &ResMut<TileMap>,
 ) -> Vec<(Position, u32)> {
-   let &Position { x, y } = position;
-   let mut weights = Vec::<(Position, u32)>::new();
-   for next_x in [-1, 0, 1].iter() {
-      for next_y in [-1, 0, 1].iter() {
-         let next_position = Position {
-            x: x + next_x,
-            y: y + next_y,
-         };
-         let tile_weight = tile_weight(next_position, tilemap);
-         if tile_weight != u32::MAX {
-            weights.push((next_position, tile_weight));
-         }
-      }
-   }
-   return weights;
+    let &Position { x, y } = position;
+    let mut weights = Vec::<(Position, u32)>::new();
+    for next_x in &[-1, 0, 1] {
+        for next_y in &[-1, 0, 1] {
+            let next_position = Position {
+                x: x + next_x,
+                y: y + next_y,
+            };
+            let tile_weight = tile_weight(next_position, tilemap);
+            if tile_weight != u32::MAX {
+                weights.push((next_position, tile_weight));
+            }
+        }
+    }
+    weights
 }
 
 fn tile_weight(
-   position: Position,
-   tilemap: &ResMut<TileMap>,
+    position: Position,
+    tilemap: &ResMut<TileMap>,
 ) -> u32 {
-   let mut weight = 1;
-   if tilemap.map.contains_key(&position) {
-      match tilemap.map.get(&position).unwrap() {
-         Tile {
-            ground_type: GroundType::Obstacle,
-            ..
-         } => weight = u32::MAX,
-         Tile {
-            ground_type: GroundType::Street,
-            ..
-         } => weight = 10,
-         _ => weight = 1,
-      }
-   }
-   weight
+    let mut weight = 1;
+    if tilemap.map.contains_key(&position) {
+        match tilemap.map[&position] {
+            Tile {
+                ground_type: GroundType::Obstacle,
+                ..
+            } => weight = u32::MAX,
+            Tile {
+                ground_type: GroundType::Street,
+                ..
+            } => weight = 10,
+            _ => weight = 1,
+        }
+    }
+    weight
 }
--- src/main.rs
+++ src/main.rs
@@ -13,135 +13,135 @@ use rand::Rng;
 extern crate engine;
 
 fn main() {
-   App::build()
-      .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.1)))
-      .add_plugin(FrameTimeDiagnosticsPlugin::default())
-      .add_plugin(LogDiagnosticsPlugin::default())
-      .add_plugins(DefaultPlugins)
-      .add_plugin(engine::movement::GraphicsPlugin)
-      .add_plugin(engine::movement::movement::MovementPlugin)
-      .add_plugin(engine::world::WorldPlugin)
-      .add_plugin(TilemapPlugin)
-      .add_plugin(TiledMapPlugin)
-      .add_startup_system(add_people.system())
-      .add_system(inspect.system())
-      .add_system(new_destination.system())
-      .run();
+    App::build()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.1)))
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugins(DefaultPlugins)
+        .add_plugin(engine::movement::GraphicsPlugin)
+        .add_plugin(engine::movement::movement::MovementPlugin)
+        .add_plugin(engine::world::WorldPlugin)
+        .add_plugin(TilemapPlugin)
+        .add_plugin(TiledMapPlugin)
+        .add_startup_system(add_people.system())
+        .add_system(inspect.system())
+        .add_system(new_destination.system())
+        .run();
 }
 
 fn add_people(
-   mut commands: Commands,
-   mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-   asset_server: Res<AssetServer>,
+    mut commands: Commands,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
 ) {
-   let mut x = 0;
+    let mut x = 0;
 
-   while x < 50 {
-      let xrange = RangeInclusive::new(0, 180);
-      let yrange = xrange.clone();
-      let mut rng = rand::thread_rng();
-      let position = engine::world::Position {
-         x: rng.gen_range(xrange),
-         y: rng.gen_range(yrange),
-      };
-      let destination =
-         engine::world::Destination(engine::world::Position { x: 60, y: 0 });
+    while x < 50 {
+        let xrange = RangeInclusive::new(0, 180);
+        let yrange = xrange.clone();
+        let mut rng = rand::thread_rng();
+        let position = engine::world::Position {
+            x: rng.gen_range(xrange),
+            y: rng.gen_range(yrange),
+        };
+        let destination =
+            engine::world::Destination(engine::world::Position { x: 60, y: 0 });
 
-      let sprite_sheet = engine::movement::init_sprite_sheet(
-         &"sprites/NPC1 (2).png".to_string(),
-         &asset_server,
-         &mut texture_atlases,
-         position,
-      );
-      engine::spawn_actor(
-         &mut commands,
-         engine::Identity {
-            specific: true,
-            name:     "Grumph Torgi".to_string(),
-         },
-         position,
-         destination,
-         sprite_sheet,
-      );
-      x += 1;
-   }
-   while x < 100 {
-      let xrange = RangeInclusive::new(-90, 90);
-      let yrange = xrange.clone();
-      let mut rng = rand::thread_rng();
-      let position = engine::world::Position {
-         x: rng.gen_range(xrange),
-         y: rng.gen_range(yrange),
-      };
+        let sprite_sheet = engine::movement::init_sprite_sheet(
+            &"sprites/NPC1 (2).png".to_owned(),
+            &asset_server,
+            &mut texture_atlases,
+            position,
+        );
+        engine::spawn_actor(
+            &mut commands,
+            engine::Identity {
+                specific: true,
+                name:     "Grumph Torgi".to_owned(),
+            },
+            position,
+            destination,
+            sprite_sheet,
+        );
+        x += 1;
+    }
+    while x < 100 {
+        let xrange = RangeInclusive::new(-90, 90);
+        let yrange = xrange.clone();
+        let mut rng = rand::thread_rng();
+        let position = engine::world::Position {
+            x: rng.gen_range(xrange),
+            y: rng.gen_range(yrange),
+        };
 
-      let destination =
-         engine::world::Destination(engine::world::Position { x: 0, y: 0 });
+        let destination =
+            engine::world::Destination(engine::world::Position { x: 0, y: 0 });
 
-      let sprite_sheet = engine::movement::init_sprite_sheet(
-         &"sprites/NPC1 (2).png".to_string(),
-         &asset_server,
-         &mut texture_atlases,
-         position,
-      );
-      engine::spawn_actor(
-         &mut commands,
-         engine::Identity {
-            specific: true,
-            name:     "Grumph Torgi".to_string(),
-         },
-         position,
-         destination,
-         sprite_sheet,
-      );
-      x += 1;
-   }
-   println!("Spawned {} entities.", x);
+        let sprite_sheet = engine::movement::init_sprite_sheet(
+            &"sprites/NPC1 (2).png".to_owned(),
+            &asset_server,
+            &mut texture_atlases,
+            position,
+        );
+        engine::spawn_actor(
+            &mut commands,
+            engine::Identity {
+                specific: true,
+                name:     "Grumph Torgi".to_owned(),
+            },
+            position,
+            destination,
+            sprite_sheet,
+        );
+        x += 1;
+    }
+    println!("Spawned {} entities.", x);
 }
 
 // This will be replaced by an agent module in the near future
 fn new_destination(
-   mut commands: Commands,
-   query: Query<
-      Entity,
-      (
-         With<engine::world::Position>,
-         Without<engine::world::Destination>,
-      ),
-   >,
+    mut commands: Commands,
+    query: Query<
+        Entity,
+        (
+            With<engine::world::Position>,
+            Without<engine::world::Destination>,
+        ),
+    >,
 ) {
-   for entity in query.iter() {
-      let xrange = RangeInclusive::new(0, 100);
-      let yrange = xrange.clone();
-      let mut rng = rand::thread_rng();
-      let x = rng.gen_range(xrange);
-      let y = rng.gen_range(yrange);
-      let destination =
-         engine::world::Destination(engine::world::Position { x, y });
-      commands.entity(entity).insert(destination);
-   }
+    for entity in query.iter() {
+        let xrange = RangeInclusive::new(0, 100);
+        let yrange = xrange.clone();
+        let mut rng = rand::thread_rng();
+        let x = rng.gen_range(xrange);
+        let y = rng.gen_range(yrange);
+        let destination =
+            engine::world::Destination(engine::world::Position { x, y });
+        commands.entity(entity).insert(destination);
+    }
 }
 
 fn inspect(
-   keyboard: Res<Input<KeyCode>>,
-   all_entities: Query<Entity>,
-   entities: &Entities,
-   archetypes: &Archetypes,
-   components: &Components,
+    keyboard: Res<Input<KeyCode>>,
+    all_entities: Query<Entity>,
+    entities: &Entities,
+    archetypes: &Archetypes,
+    components: &Components,
 ) {
-   if keyboard.just_pressed(KeyCode::F1) {
-      for entity in all_entities.iter() {
-         println!("Entity: {:?}", entity);
-         if let Some(entity_location) = entities.get(entity) {
-            if let Some(archetype) =
-               archetypes.get(entity_location.archetype_id)
-            {
-               for component in archetype.components() {
-                  if let Some(info) = components.get_info(component) {
-                     println!("\tComponent: {}", info.name());
-                  }
-               }
+    if keyboard.just_pressed(KeyCode::F1) {
+        for entity in all_entities.iter() {
+            println!("Entity: {:?}", entity);
+            if let Some(entity_location) = entities.get(entity) {
+                if let Some(archetype) =
+                    archetypes.get(entity_location.archetype_id)
+                {
+                    for component in archetype.components() {
+                        if let Some(info) = components.get_info(component) {
+                            println!("\tComponent: {}", info.name());
+                        }
+                    }
+                }
             }
-         }
-      }
-   }
+        }
+    }
 }
