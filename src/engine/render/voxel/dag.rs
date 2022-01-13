e9e10634bb93882d250e614734c5f4b04c5807c3
Samuel Casavant <samuel.j.casavant@gmail.com>
2022-01-13 16:57:33 -0500

    Working (?) DAG implementation

--- src/engine/render/voxel/dag.rs
+++ src/engine/render/voxel/dag.rs
@@ -1,13 +1,61 @@
 pub struct ChunkDag {
-    pub root:    Node,
-    pub ambient: u16,
+    root: Children,
+}
+
+// Boxcar Children:
+type Children = [Option<Box<Node>>; 8];
+
+struct Node {
+    // Child indices:
+    // 0: -x, -y, -z
+    // 1: +x, -y, -z
+    // 2: -x, +y, -z
+    // 3: +x, +y, -z
+    // 4: -x, -y, +z
+    // 5: +x, -y, +z
+    // 6: -x, +y, +z
+    // 7: +x, +y, +z
+    val:      u16,
+    children: Children,
+}
+
+impl Node {
+    pub fn add_child(
+        &mut self,
+        val: u16,
+        index: usize,
+    ) -> Option<&mut Box<Node>> {
+        let new_children: [Option<Box<Node>>; 8] = Default::default();
+        let new_node = Box::new(Node {
+            val,
+            children: new_children,
+        });
+        self.children[index] = Some(new_node);
+        self.children[index].as_mut()
+    }
+    pub fn get_child(
+        &self,
+        index: usize,
+    ) -> Option<&Box<Node>> {
+        self.children[index].as_ref()
+    }
+    pub fn get_child_mut(
+        &mut self,
+        index: usize,
+    ) -> Option<&mut Box<Node>> {
+        self.children[index].as_mut()
+    }
+    pub fn get_val_mut(
+        &mut self,
+        index: usize,
+    ) -> Option<&mut u16> {
+        self.children[index].as_mut().map(|node| &mut node.val)
+    }
 }
 impl ChunkDag {
-    pub fn new(ambient: u16) -> ChunkDag {
-        ChunkDag {
-            root: Node::new(),
-            ambient,
-        }
+    pub fn new() -> Self {
+        let root: [Option<Box<Node>>; 8] = Default::default();
+        ChunkDag { root }
     }
     pub fn get_pos(
         &self,
@@ -16,43 +64,33 @@ impl ChunkDag {
         mut y: i16,
         mut z: i16,
     ) -> u16 {
-        let mut node = &self.root;
-        let mut range = i16::MAX;
+        let child_index =
+            (x > 0) as usize + (y > 0) as usize * 2 + (z > 0) as usize * 4;
+
+        let mut node = self.root[child_index].as_ref().unwrap();
+
+        let mut range = i16::MAX / 2;
+        x = if x > 0 { x - range } else { x + range };
+        y = if y > 0 { y - range } else { y + range };
+        z = if z > 0 { z - range } else { z + range };
+
         loop {
-            range = range / 2;
-            if range == 4 {
-                return node.value;
+            let child_index = (x > 0) as usize
+                + (y > 0) as usize * 2
+                + (z > 0) as usize * 4;
+
+            range /= 2;
+            x = if x > 0 { x - range } else { x + range };
+            y = if y > 0 { y - range } else { y + range };
+            z = if z > 0 { z - range } else { z + range };
+
+            if let Some(child) = node.get_child(child_index) {
+                node = child;
             } else {
-                let mut child_index = 0;
-                if x > 0 {
-                    child_index += 1;
-                    x = x + range;
-                } else {
-                    x = x - range;
-                }
-                if y > 0 {
-                    child_index += 2;
-                    y = y + range;
-                } else {
-                    y = y - range;
-                }
-                if z > 0 {
-                    child_index += 4;
-                    z = z + range;
-                } else {
-                    z = z - range;
-                }
-                if let Some(child) =
-                    node.children[child_index as usize].as_ref()
-                {
-                    node = child;
-                } else {
-                    return self.ambient;
-                }
+                return node.val;
             }
         }
     }
-
     fn set_pos(
         &mut self,
         mut x: i16,
@@ -60,79 +98,37 @@ impl ChunkDag {
         mut z: i16,
         value: u16,
     ) {
-        let mut node = &mut self.root;
-        let mut range = i16::MAX;
+        let child_index =
+            (x > 0) as usize + (y > 0) as usize * 2 + (z > 0) as usize * 4;
+
+        let mut node = self.root[child_index].as_mut().unwrap();
+
+        let mut range = i16::MAX / 2;
+        x = if x > 0 { x - range } else { x + range };
+        y = if y > 0 { y - range } else { y + range };
+        z = if z > 0 { z - range } else { z + range };
+
         loop {
-            range = range / 2;
-            if range == 4 {
-                node.set_value(value);
-                return;
-            } else {
-                let mut child_index = 0;
-                if x > 0 {
-                    child_index += 1;
-                    x = x + range;
+            let child_index = (x > 0) as usize
+                + (y > 0) as usize * 2
+                + (z > 0) as usize * 4;
+
+            range /= 2;
+            x = if x > 0 { x - range } else { x + range };
+            y = if y > 0 { y - range } else { y + range };
+            z = if z > 0 { z - range } else { z + range };
+
+            if node.get_child_mut(child_index).is_some() {
+                let child = node.get_child_mut(child_index).unwrap();
+                if range == 4 {
+                    child.val = value;
+                    return;
                 } else {
-                    x = x - range;
-                }
-                if y > 0 {
-                    child_index += 2;
-                    y = y + range;
-                } else {
-                    y = y - range;
-                }
-                if z > 0 {
-                    child_index += 4;
-                    z = z + range;
-                } else {
-                    z = z - range;
-                }
-                if let Some(child) = &node.children[child_index as usize] {
                     node = child;
-                } else {
-                    // TODO: Create branch to position
                 }
+            } else {
+                node = node.add_child(value, child_index).unwrap()
             }
         }
-    }
-}
-
-pub struct Node {
-    // Child indices:
-    // 0: x < 0, y < 0, z < 0
-    // 1: x > 0, y < 0, z < 0
-    // 2: x < 0, y > 0, z < 0
-    // 3: x > 0, y > 0, z < 0
-    // 4: x < 0, y < 0, z > 0
-    // 5: x > 0, y < 0, z > 0
-    // 6: x < 0, y > 0, z > 0
-    // 7: x > 0, y > 0, z > 0
-    pub children: Vec<Option<Node>>,
-    pub value:    u16,
-}
-impl Node {
-    pub fn new() -> Node {
-        let mut children = Vec::new();
-        children.resize_with(8, || None);
-        Node { children, value: 0 }
-    }
-    pub fn add_child(
-        &mut self,
-        child: Node,
-        position: usize,
-    ) {
-        if position >= 8 {
-            panic!(
-                "Attempted to add child to nonexistant index: {:?}",
-                position
-            )
-        }
-        self.children[position] = Some(child);
-    }
-    pub fn set_value(
-        &mut self,
-        value: u16,
-    ) {
-        self.value = value;
     }
 }
